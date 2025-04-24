ad733adc2b127a1a7380e0394f3149665160e7a4
Ayman-121 <107408891+Ayman-121@users.noreply.github.com>
2025-04-24 11:45:52 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,86 +1,86 @@
 const projects = [
-  {
-      id: 1,
-      title: "My Info <sup>v1</sub> <sup>2021</sub>",
-      description: "My previous personal website. Responsive, Clean, and Simple.",
-      image: "imgs/My_Info.jpeg",
-      url: "https://ayman-121.github.io/My_Info/",
-      category: "portfolio",
-      tags: ["HTML5", "CSS3", "JavaScript"]
-  },
-  {
-      id: 2,
-      title: "QQQ Media <sub>Mock</sub> <sup>2021</sub>",
-      description: "A media company website with modern design and interactive elements. This project demonstrates advanced CSS/JS animations, video integration, and a content management system.",
-      image: "imgs/QQQ.jpeg",
-      url: "https://ayman-121.github.io/QQQ/",
-      category: "web",
-      tags: ["HTML5", "CSS3", "JavaScript", "API"]
-  },
-  {
-      id: 3,
-      title: "E-School Platform <sub>Mock</sub> <sup>2021</sub>",
-      description: "Similar tp Eschool's Front-end student's page.",
-      image: "imgs/Eschool.jpeg",
-      url: "https://ayman-121.github.io/Eschool/",
-      category: "web",
-      tags: ["HTML5", "CSS3", "JavaScript"]
-  },
-  {
-      id: 4,
-      title: "Athlete Portfolio <sup>2021</sub>",
-      description: "A professional portfolio website for an athlete showcasing career highlights, skills, and achievements. Features a timeline of events, media gallery, and contact information.",
-      image: "imgs/Sharara.jpeg",
-      url: "https://ayman-121.github.io/Sharara_Mohammad/",
-      category: "portfolio",
-      tags: ["HTML5", "CSS3", "JavaScript"]
-  },
-  {
-      id: 5,
-      title: "Mexant Business Solutions <sub>A Test Website</sub> <sup>2020</sub>",
-      description: "A business services website offering web development, design, and digital marketing solutions.",
-      image: "imgs/Mexant.jpeg",
-      url: "https://ayman-121.github.io/Mexant/",
-      category: "web",
-      tags: ["HTML5", "SCSS", "JavaScript"]
-  },
-  {
-      id: 6,
-      title: "Quran Website <sup>v1</sup> <sup>2021</sub>",
-      description: "An interactive Quran Website with verses. Developed with a focus on performance.",
-      image: "imgs/quran.jpeg",
-      url: "https://ayman-121.github.io/Quran/",
-      category: "web",
-      tags: ["JavaScript", "HTML5", "API"]
-  },
-  {
-      id: 7,
-      title: "Math Arithmetic Sequences Calculator <sup>2024</sub>",
-      description: "A specialized tool for mathematical calculations and sequence analysis.",
-      image: "imgs/maths.jpeg",
-      url: "https://ayman-121.github.io/math-project-ayman/",
-      category: "web",
-      tags: ["JavaScript", "HTML5", "CSS3"]
-  },
-  {
-      id: 8,
-      title: "Al Logarithms Explorer <sup>2025</sub>",
-      description: "A specialized tool for Visualize, understand, and master logarithmic functions. Features include graphing capabilities and formula references for educational purposes.",
-      image: "imgs/math-2.jpeg",
-      url: "https://ayman-121.github.io/math-proj-2/",
-      category: "web",
-      tags: ["JavaScript", "HTML5", "CSS3"]
-  },
     {
-      id: 9,
-      title: "KKIS MUN Website <sup>2025</sub>",
-      description: "The official website for the Knights of Knowledge International School (KKIS) Model United Nations 2025. This site provides all essential information about the KKIS MUN 2025 conference, including registration details, committee topics, schedules, and more. Built with a modern web stack to ensure accessibility and responsiveness.",
-      image: "imgs/math-2.jpeg",
-      url: "https://kkis-online.github.io/kkis-mun-25/",
-      category: "web",
-      tags: ["JavaScript", "HTML5", "CSS3"]
-  }
-];
+        id: 1,
+        title: "My Info <sup>v1</sub> <sup>2021</sub>",
+        description: "My previous personal website. Responsive, Clean, and Simple.",
+        image: "imgs/My_Info.jpeg",
+        url: "https://ayman-121.github.io/My_Info/",
+        category: "portfolio",
+        tags: ["HTML5", "CSS3", "JavaScript"]
+    },
+    {
+        id: 2,
+        title: "QQQ Media <sub>Mock</sub> <sup>2021</sub>",
+        description: "A media company website with modern design and interactive elements. This project demonstrates advanced CSS/JS animations, video integration, and a content management system.",
+        image: "imgs/QQQ.jpeg",
+        url: "https://ayman-121.github.io/QQQ/",
+        category: "web",
+        tags: ["HTML5", "CSS3", "JavaScript", "API"]
+    },
+    {
+        id: 3,
+        title: "E-School Platform <sub>Mock</sub> <sup>2021</sub>",
+        description: "Similar tp Eschool's Front-end student's page.",
+        image: "imgs/Eschool.jpeg",
+        url: "https://ayman-121.github.io/Eschool/",
+        category: "web",
+        tags: ["HTML5", "CSS3", "JavaScript"]
+    },
+    {
+        id: 4,
+        title: "Athlete Portfolio <sup>2021</sub>",
+        description: "A professional portfolio website for an athlete showcasing career highlights, skills, and achievements. Features a timeline of events, media gallery, and contact information.",
+        image: "imgs/Sharara.jpeg",
+        url: "https://ayman-121.github.io/Sharara_Mohammad/",
+        category: "portfolio",
+        tags: ["HTML5", "CSS3", "JavaScript"]
+    },
+    {
+        id: 5,
+        title: "Mexant Business Solutions <sub>A Test Website</sub> <sup>2020</sub>",
+        description: "A business services website offering web development, design, and digital marketing solutions.",
+        image: "imgs/Mexant.jpeg",
+        url: "https://ayman-121.github.io/Mexant/",
+        category: "web",
+        tags: ["HTML5", "SCSS", "JavaScript"]
+    },
+    {
+        id: 6,
+        title: "Quran Website <sup>v1</sup> <sup>2021</sub>",
+        description: "An interactive Quran Website with verses. Developed with a focus on performance.",
+        image: "imgs/quran.jpeg",
+        url: "https://ayman-121.github.io/Quran/",
+        category: "web",
+        tags: ["JavaScript", "HTML5", "API"]
+    },
+    {
+        id: 7,
+        title: "Math Arithmetic Sequences Calculator <sup>2024</sub>",
+        description: "A specialized tool for mathematical calculations and sequence analysis.",
+        image: "imgs/maths.jpeg",
+        url: "https://ayman-121.github.io/math-project-ayman/",
+        category: "web",
+        tags: ["JavaScript", "HTML5", "CSS3"]
+    },
+    {
+        id: 8,
+        title: "Al Logarithms Explorer <sup>2025</sub>",
+        description: "A specialized tool for Visualize, understand, and master logarithmic functions. Features include graphing capabilities and formula references for educational purposes.",
+        image: "imgs/math-2.jpeg",
+        url: "https://ayman-121.github.io/math-proj-2/",
+        category: "web",
+        tags: ["JavaScript", "HTML5", "CSS3"]
+    },
+      {
+        id: 9,
+        title: "KKIS MUN Website <sup>2025</sub>",
+        description: "The official website for the Knights of Knowledge International School (KKIS) Model United Nations 2025. This site provides all essential information about the KKIS MUN 2025 conference, including registration details, committee topics, schedules, and more. Built with a modern web stack to ensure accessibility and responsiveness.",
+        image: "imgs/mun.png",
+        url: "https://kkis-online.github.io/kkis-mun-25/",
+        category: "web",
+        tags: ["JavaScript", "HTML5", "CSS3"]
+    }
+  ];
 
 document.addEventListener("DOMContentLoaded", function() {
   // DOM Elements
